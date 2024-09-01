876f45e0e97109d016022536e856ed81e72de9b8
Ross Devine <roscod@live.ie>
2024-09-01 14:32:28 +0100

    Fix change to ProtectedLayout component

--- src/components/ProtectedLayout/ProtectedLayout.jsx
+++ src/components/ProtectedLayout/ProtectedLayout.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { Outlet, Navigate } from 'react-router-dom';
 import { useAuth } from '../../context/AuthContext';
+import LoadingScreen from '../LoadingScreen/LoadingScreen';
 
 const ProtectedLayout = () => {
-  const { currentUser } = useAuth();
+  const { currentUser, loading } = useAuth();
+
+  if (loading) {
+    return <LoadingScreen />;
+  }
 
   if (!currentUser) {
     return <Navigate to="/login" />;
